71be3da28c1b31a39d40895473f5eef4d1260253
sintanbabo <sintanbabo@gmail.com>
2020-01-24 22:20:51 +0900

    Add CallByXXXTest

---
+++ src/CallByValue/CallByXXXTest.java
@@ -0,0 +1,14 @@
+public class CallByXXXTest
+{
+	public static void assignNewPerson(Person p)
+	{
+		p.setName("duna");
+	}
+
+	public static void main(String[] args)
+	{
+		Person a = new Person("sintanbabo");
+		assignNewPerson(a);
+		System.out.println(a);
+	}
+}
